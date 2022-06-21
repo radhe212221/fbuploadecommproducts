15ad11ebdf96102a1c062da635f70c65a71c1f43
radhe212221 <radhe212221@gmail.com>
2022-06-21 15:09:52 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route,Link } from 'react-router-dom';
+import './style.css';
+import Home from './Home';
+import Upload from './Upload';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <BrowserRouter>
+      <Link to="/">home</Link>
+      <Link to="/upload">upload</Link>
+      <Routes>
+        <Route path="" index element={<Home />} />
+        <Route path="upload" index element={<Upload />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const url='https://june3-5cc25-default-rtdb.firebaseio.com/'
